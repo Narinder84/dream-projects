d3a8bd325f99f4786f6f40e1f1ab5d5548098ccf
getdev84 <getdev84@gmail.com>
2021-04-22 20:06:49 -0400

    implemented hooks in app

--- src/components/darkLightMode/darkLightComponents/lightDark.contacts/lightDark.contacts.component.js
+++ src/components/darkLightMode/darkLightComponents/lightDark.contacts/lightDark.contacts.component.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { send } from 'emailjs-com';
 
 import contactDay from '../../../../img/undraw_contact_us_day.svg';
@@ -8,112 +8,122 @@ import contactNight from '../../../../img/undraw_contact_us_night.svg';
 
 import './lightDark.contacts.styles.css';
 
-class LightDarkConstactSection extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			ismailsent: '',
-			toSend: {
-				name: '',
-				email: '',
-				subject: '',
-				message: '',
-			},
-		};
-	}
-	handleInputChange = (e) => {
-		const { name, value } = e.target;
-		const newToSend = { ...this.state.toSend, [name]: value };
-		this.setState({ ...this.state, toSend: newToSend });
+const LightDarkConstactSection = (props) => {
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		ismailsent: '',
+	// 		toSend: {
+	// 			name: '',
+	// 			email: '',
+	// 			subject: '',
+	// 			message: '',
+	// 		},
+	// 	};
+	// }
+
+	const INITstate = {
+		ismailsent: '',
+		toSend: {
+			name: '',
+			email: '',
+			subject: '',
+			message: '',
+		},
 	};
-	handleSubmit = async () => {
-		const { name, email, subject, message } = this.state.toSend;
-		console.log(this.state);
+	const [state, setState] = useState(INITstate);
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		const newToSend = { ...state.toSend, [name]: value };
+		setState({ ...state, toSend: newToSend });
+	};
+	const handleSubmit = async () => {
+		const { name, email, subject, message } = state.toSend;
+		console.log(state);
 		// sendMail();
 		if (name && email && subject && message) {
 			return send(
 				'service_g34bwmw',
 				'template_i2iopti',
-				this.state.toSend,
+				state.toSend,
 
 				'user_nOccccZao1z8VOViku901',
 			)
 				.then((response) => {
 					// console.log('SUCCESS!', response.status, response.text);
 					const values = { name: '', email: '', subject: '', message: '' };
-					this.setState({ ...this.state, ismailsent: true, toSend: values });
+					setState({ ...state, ismailsent: true, toSend: values });
 				})
 				.catch((err) => {
 					// console.log('FAILED...', err);
-					this.setState({ ismailsent: false });
+					setState({ ismailsent: false });
 				});
 		}
-		this.setState({ ismailsent: false });
+		setState({ ismailsent: false });
 	};
-	render() {
-		const isLightMode = this.props.isLightMode;
-		console.log('====> ', isLightMode, isLightMode ? 'Day' : 'contactNight');
-		return (
-			<section id='contact'>
-				<h1>Contact</h1>
-				<img
-					src={isLightMode ? contactDay : contactNight}
-					alt='Idea'
-					id='image3'
-				/>
-				<div className='image-container' id='contact-form'>
-					<div className='message-container'>
-						{this.state.ismailsent === '' ? (
-							<p id='hd'>Send Message</p>
-						) : this.state.ismailsent === true ? (
-							<p className='hd' id='success'>
-								Message has been sent. Thanks for showing intrest.
-							</p>
-						) : this.state.ismailsent === false ? (
-							<p className='hd' id='fault'>
-								Somethig missing.
-							</p>
-						) : null}
-					</div>
-					<p className='form-p'>Name: </p>
-					<input
-						type='text'
-						name='name'
-						onChange={this.handleInputChange}
-						value={this.state.toSend.name}
-					/>
-					<p className='form-p'>Email:</p>
-					<input
-						type='email'
-						name='email'
-						onChange={this.handleInputChange}
-						value={this.state.toSend.email}
-					/>
-					<p className='form-p'>Subject:</p>
-					<input
-						type='text'
-						name='subject'
-						onChange={this.handleInputChange}
-						value={this.state.toSend.subject}
-					/>
-					<p className='form-p'>Your Message:</p>
 
-					<textarea
-						id='w3review'
-						name='message'
-						value={this.state.toSend.message}
-						rows='4'
-						cols='50'
-						onChange={this.handleInputChange}></textarea>
-					<div className='send-container'>
-						<button className='primary' id='send' onClick={this.handleSubmit}>
-							Send
-						</button>
-					</div>
+	const isLightMode = props.isLightMode;
+	console.log('====> ', isLightMode, isLightMode ? 'Day' : 'contactNight');
+	return (
+		<section id='contact'>
+			<h1>Contact</h1>
+			<img
+				src={isLightMode ? contactDay : contactNight}
+				alt='Idea'
+				id='image3'
+			/>
+			<div className='image-container' id='contact-form'>
+				<div className='message-container'>
+					{state.ismailsent === '' ? (
+						<p id='hd'>Send Message</p>
+					) : state.ismailsent === true ? (
+						<p className='hd' id='success'>
+							Message has been sent. Thanks for showing intrest.
+						</p>
+					) : state.ismailsent === false ? (
+						<p className='hd' id='fault'>
+							Somethig missing.
+						</p>
+					) : null}
 				</div>
-			</section>
-		);
-	}
-}
+				<p className='form-p'>Name: </p>
+				<input
+					type='text'
+					name='name'
+					onChange={handleInputChange}
+					value={state.toSend.name}
+				/>
+				<p className='form-p'>Email:</p>
+				<input
+					type='email'
+					name='email'
+					onChange={handleInputChange}
+					value={state.toSend.email}
+				/>
+				<p className='form-p'>Subject:</p>
+				<input
+					type='text'
+					name='subject'
+					onChange={handleInputChange}
+					value={state.toSend.subject}
+				/>
+				<p className='form-p'>Your Message:</p>
 
-export default LightDarkConstactSection;
+				<textarea
+					id='w3review'
+					name='message'
+					value={state.toSend.message}
+					rows='4'
+					cols='50'
+					onChange={handleInputChange}></textarea>
+				<div className='send-container'>
+					<button className='primary' id='send' onClick={handleSubmit}>
+						Send
+					</button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default React.memo(LightDarkConstactSection);
